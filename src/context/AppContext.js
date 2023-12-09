94a5106f6162a144627f25b894d48b6349c88b00
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-12-09 16:08:41 -0300

    Criando um contexto de aplicativo - Divisão de código com React Suspense

---
+++ src/context/AppContext.js
@@ -0,0 +1,10 @@
+import { ListsContextProvider } from './ListsContext';
+import { ItemsContextProvider } from './ItemsContext';
+const AppContext = ({ children }) => {
+  return (
+    <ListsContextProvider>
+      <ItemsContextProvider>{children}</ItemsContextProvider>
+    </ListsContextProvider>
+  );
+};
+export default AppContext;
